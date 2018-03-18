bbd74e6b86334175953fc0309812cb7c5742d1a8
rmpestano <rmpestano@gmail.com>
2018-03-18 03:54:51 -0300

    fixes #20

--- src/main/java/com/github/adminfaces/showcase/bean/LayoutMB.java
+++ src/main/java/com/github/adminfaces/showcase/bean/LayoutMB.java
@@ -30,6 +30,10 @@ public class LayoutMB implements Serializable {
         layout = "/WEB-INF/templates/template-top.xhtml";
     }
 
+    public void test() {
+        layout = "/WEB-INF/templates/template-top.xhtml";
+    }
+
     public void setDefaultLayout() {
         layout = "/WEB-INF/templates/template.xhtml";
     }
