f332b9f9eced3477870069ea7e3cdf14c33be630
rafael-pestano <rmpestano@gmail.com>
2018-04-16 11:35:58 -0300

    refs https://github.com/adminfaces/admin-template/issues/56

--- src/main/webapp/sources/com/github/adminfaces/template/bean/LayoutMB.java
+++ src/main/webapp/sources/com/github/adminfaces/template/bean/LayoutMB.java
@@ -1,6 +1,5 @@
 package com.github.adminfaces.template.bean;
 
-import com.github.adminfaces.showcase.bean.*;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -9,7 +8,7 @@ import java.io.Serializable;
 /**
  * Created by rmpestano on 07/05/17.
  */
-@Named("adminLayoutMB")
+@Named
 @SessionScoped
 public class LayoutMB implements Serializable {
 
