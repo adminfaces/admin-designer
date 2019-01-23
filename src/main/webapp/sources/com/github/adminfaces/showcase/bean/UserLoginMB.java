f157df40f7466074ad0e1da79bbe3688df336be3
rafael-pestano <rafael-pestano@procergs.rs.gov.br>
2019-01-23 18:20:04 -0200

    Upgrade to PF 7.0-SNAPSHOT

--- src/main/webapp/sources/com/github/adminfaces/showcase/bean/UserLoginMB.java
+++ src/main/webapp/sources/com/github/adminfaces/showcase/bean/UserLoginMB.java
@@ -16,7 +16,6 @@
 package com.github.adminfaces.showcase.bean;
 
 import org.omnifaces.cdi.ViewScoped;
-
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
