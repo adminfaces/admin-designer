dca3ad94a8adc2c6be81c70fd846c07002c2ab73
rafael-pestano <rafael-pestano@procergs.rs.gov.br>
2019-02-05 11:34:35 -0200

    Sync with theme and template

--- src/main/webapp/resources/js/admintemplate.js
+++ src/main/webapp/resources/js/admintemplate.js
@@ -20,6 +20,12 @@ function activateRippleIcons() {
     }).on('mouseup mouseleave touchend touchcancel', '.ui-messages .ui-icon, .ui-growl-item .ui-icon, span.ui-tree-toggler, span.ui-icon-calendar, div.ui-selectcheckboxmenu-trigger span.ui-icon-triangle-1-s, span.ui-icon-circle-close, .ui-panel-titlebar span.ui-icon, .ui-dialog-titlebar span.ui-icon, .ui-paginator span.ui-icon, .ui-autocomplete-dropdown span.ui-icon-triangle-1-s, .ui-selectonemenu-trigger span.ui-icon-triangle-1-s, .ui-spinner-button .ui-icon', null, function (e) {
         $(this).removeClass("icon-ripple");
     });
+
+    $(document.body).on('mousedown touchstart', 'a.ui-rowgroup-toggler', null, function (e) {
+        $(this).find('.ui-rowgroup-toggler-icon').addClass("icon-ripple");
+    }).on('mouseup mouseleave touchend touchcancel', 'a.ui-rowgroup-toggler', null, function (e) {
+        $(this).find('.ui-rowgroup-toggler-icon').removeClass("icon-ripple");
+    });
 }
 
 function stripTrailingSlash(str) {
